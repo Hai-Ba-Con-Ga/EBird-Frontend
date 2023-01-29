4ef88635cfe7d92bc73659c5400c83da2d8706ec
xWyvernPx <phonglethanh2@gmail.com>
2023-01-29 13:00:26 +0700

    [FE][Config] Testing setup

--- src/_component/layout/ProtectedRoute.tsx
+++ src/_component/layout/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { AuthState, User } from "../../AuthAtom";
+import { AuthState, User } from "../../_atom/AuthAtom";
 
 type Props = {
   user?: AuthState | null;
