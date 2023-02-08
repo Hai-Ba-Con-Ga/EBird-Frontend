6441cfa5af70faf3ac48b22c224bf8a9a6fad2f3
YatinCyber <chabonglatui@gmail.com>
2023-02-08 08:43:56 +0700

    Loading demo

---
+++ src/_component/common/loading/Loading.js
@@ -0,0 +1,14 @@
+// import '../App.css';
+// function Loading (){
+//     return(
+//         <div className="music-waves-2">
+//         <span></span>
+//         <span></span>
+//         <span></span>
+//         <span></span>
+//         <span></span>
+//         <span></span>
+//         <span></span>
+// </div>
+//     )
+// }
