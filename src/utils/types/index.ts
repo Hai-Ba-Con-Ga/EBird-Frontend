3597902dcae1087c13df260eacd2ea617a58d166
YatinCyber <chabonglatui@gmail.com>
2023-02-20 14:36:41 +0700

    Type Util

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -1,18 +1,39 @@
-export enum MatchStatus {
-  Waiting,
-  Pending,
-  During,
-  Completed,
-  Cancelled,
+export interface Account {//còn thiếu nhiều
+  id:	string;
+  isDeleted:	boolean;
+  password: string;
+  email: string;
+  createDateTime:Date;
+  firstName: string;
+  lastName: string;
+  roleString: string;
+  // role: 	Roleinteger($int32)
+  // Enum:
+  // [ 0, 1 ]
+  username: string
+  description: string
 }
-export interface MatchRequest {
-  primaryBird: Bird;
-  time: string;
-  place: string;
-  secondaryBird: Bird;
+export interface AccountResource {
+  id:	string;
+  isDeleted:	boolean;
+  accountId: string;
+  account: Account; 
+  resourceId: string;
+  resource: ResourceEntity;
+}
+export interface ResourceEntity{
+  id: string;
+  isDeleted: boolean;
+  createById: string;
+  account: Account;
+  dataLink: string;
+  description: string;
+  createDate: Date;
+  //
 }
 export interface Bird {
   id: string;
+  isDeleted:	boolean;
   ratio: string;
   name: string;
   age: number;
@@ -26,3 +47,32 @@ export interface Bird {
   createdDatetime: Date;
   resourceList: any[];
 }
+export interface BirdResource {
+  id:	string;
+  isDeleted:	boolean;
+  birdId: string;
+  bird: Bird; //
+  resourceId: string;
+  //resource
+}
+
+
+
+export enum MatchStatus {
+  Waiting,
+  Pending,
+  During,
+  Completed,
+  Cancelled,
+}
+export interface MatchRequest {
+  primaryBird: Bird;
+  time: string;
+  place: string;
+  secondaryBird: Bird;
+}
+
+
+
+
+
